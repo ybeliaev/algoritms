2f3f6724f327aaab81a91c9ea71cf3537c8c6303
ybeliaev <ybeliaev@gmail.com>
2022-07-20 13:20:32 +0300

    add code bubble sort

--- script.py
+++ script.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+arr = [7, 5, -8, 10, 1]
+
+N = len(arr) # число элементов в списке
+
+for i in range(0, N-1):
+    for j in range(0, N-1-i):
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+            
+print(arr)
